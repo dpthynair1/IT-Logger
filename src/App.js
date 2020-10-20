97b121867d1008ced2488602c771390b6eb4b147
Deepthy Nair <dpthynair@gmail.com>
2020-10-20 21:33:09 +0200

    Init commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Fragment, useEffect } from 'react';
+import SearchBar from './components/layout/SearchBar'
+import Logs from './components/logs/Logs'
+
+
+import 'materialize-css/dist/css/materialize.min.css'
+import M from 'materialize-css/dist/js/materialize'
+import './App.css';
+
+const App = () =>{
+
+  useEffect(() => {
+
+    // Init materialize JS
+
+    M.AutoInit()
+  })
+  return (
+    <Fragment>
+      <SearchBar />
+      <div className="container">
+        <Logs />
+      </div>
+    </Fragment>
+  );
+}
+
+export default App;
